ca87f634a5d900cc4d3b1f59e954e8e23f5e4051
David Hán <handuy099@gmail.com>
2023-07-11 16:48:09 +0700

    fix bug

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ const port = 3000
 app.engine('.hbs', engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 app.set('views', './src/resources/views/')
-console.log(__dirname)
 
 app.use(express.static('public'))
 
